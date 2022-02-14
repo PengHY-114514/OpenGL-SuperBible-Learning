4308b9e4dffe0e6b4697f940e4a952c86718b08c
彭海洋 <53474237+PengHY-114514@users.noreply.github.com>
2022-02-15 01:23:46 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,34 @@
+#include "sb7.h"
+
+class AHelloWorld :public sb7::application
+{
+public:
+
+	void init()
+	{
+		static const char title[] = "HelloooOOOOpenGL";
+
+		sb7::application::init();
+
+		memcpy(info.title, title, sizeof(title));
+	}
+	void render(double currentTime)
+	{
+		float r = (float)sin(currentTime)*0.25f + 0.75f;
+			
+		float g = (float)cos(currentTime)*0.25f + 0.75f;
+
+		float b = (float)cos(currentTime+1)*0.25f + 0.75f;
+
+		const GLfloat color[] = { r,
+								  g,
+								  b,
+								  1.0f };
+
+		glClearBufferfv(GL_COLOR, 0, color);
+
+	}
+};
+
+DECLARE_MAIN(AHelloWorld);
+
